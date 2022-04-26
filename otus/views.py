10bf5ff9edd3660fa0d0c42f915a296d682cfdf0
Ivan.Shevchenko <krasawko@yandex.ru>
2022-04-26 12:41:57 +0300

    test docker secret

--- otus/views.py
+++ otus/views.py
@@ -4,5 +4,7 @@ import socket
 
 def index_view(request):
     name_host = socket.gethostname()
+    file = open('./test_secret.txt', 'r')
+    secret = file.read()
 
-    return render(request, 'otus/index.html', {'name_host': name_host})
+    return render(request, 'otus/index.html', {'name_host': name_host, 'secret': secret})
