407bc69350712f50beeef9424777897aeb6b8ca5
IvSh <example@exmp.com>
2022-05-06 15:54:05 +0300

    Added frontsfgsd

--- otus/views.py
+++ otus/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import socket
+import os
 
 
 def index_view(request):
     name_host = socket.gethostname()
     file = open('./test_secret.txt', 'r')
     secret = file.read()
+    env_sec = os.environ['APPDATA']
 
-    return render(request, 'otus/index.html', {'name_host': name_host, 'secret': secret})
+    return render(request, 'otus/index.html', {'name_host': name_host, 'secret': secret, 'secret_env': env_sec})
