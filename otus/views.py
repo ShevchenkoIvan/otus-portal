c314466080c076b1ab4b2a99181ee0548a249139
IvSh <example@exmp.com>
2022-05-17 10:17:36 +0300

    Changes

--- otus/views.py
+++ otus/views.py
@@ -6,7 +6,15 @@ import os
 def index_view(request):
     name_host = socket.gethostname()
     file = open('./test_secret.txt', 'r')
+    config_file = open('./test_config.txt', 'r')
     secret = file.read()
+    config = config_file.read()
     env_sec = os.environ.get('MY_ENV')
+    context = {
+        'name_host': name_host,
+        'secret': secret,
+        'secret_env': env_sec,
+        'config': config,
+    }
 
-    return render(request, 'otus/index.html', {'name_host': name_host, 'secret': secret, 'secret_env': env_sec})
+    return render(request, 'otus/index.html', context)
